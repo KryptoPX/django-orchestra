KryptoPX/django-orchestra

commit 7d975637d53d3711b5f8fbe49241d685c4bcf98d

Replace ForeignKey.field.rel.to --> field.model
rel.to dropped on Django 2.0

orchestra/contrib/accounts/actions.py
@@ -175,7 +175,7 @@ def delete_related_services(modeladmin, request, queryset):
     for model, objs in collector.model_objs.items():
         count = 0
         # discount main systemuser
-        if model is modeladmin.model.main_systemuser.field.rel.to:
+        if model is modeladmin.model.main_systemuser.field.model:
             count = len(objs) - 1
         # Discount account
         elif model is not modeladmin.model and model in registered_services:

orchestra/contrib/accounts/forms.py
@@ -34,7 +34,7 @@ def create_account_creation_form():
             fields[field_name] = forms.BooleanField(
                 initial=True, required=False, label=label, help_text=help_text)
             create_related.append((model, key, kwargs, help_text))
-        
+
     def clean(self, create_related=create_related):
         """ unique usernames between accounts and system users """
         cleaned_data = UserCreationForm.clean(self)
@@ -47,7 +47,7 @@ def create_account_creation_form():
             # Previous validation error
             return
         errors = {}
-        systemuser_model = Account.main_systemuser.field.rel.to
+        systemuser_model = Account.main_systemuser.field.model
         if systemuser_model.objects.filter(username=account.username).exists():
             errors['username'] = _("A system user with this name already exists.")
         for model, key, related_kwargs, __ in create_related:
@@ -62,11 +62,11 @@ def create_account_creation_form():
                     params={'type': verbose_name})
         if errors:
             raise ValidationError(errors)
-    
+
     def save_model(self, account):
         enable_systemuser=self.cleaned_data['enable_systemuser']
         account.save(active_systemuser=enable_systemuser)
-    
+
     def save_related(self, account):
         for model, key, related_kwargs, __ in settings.ACCOUNTS_CREATE_RELATED:
             model = apps.get_model(model)
@@ -76,14 +76,14 @@ def create_account_creation_form():
                     key: eval(value, {'account': account}) for key, value in related_kwargs.items()
                 }
                 model.objects.create(account=account, **kwargs)
-    
+
     fields.update({
         'create_related_fields': list(fields.keys()),
         'clean': clean,
         'save_model': save_model,
         'save_related': save_related,
     })
-    
+
     return type('AccountCreationForm', (UserCreationForm,), fields)
 
 

orchestra/contrib/bills/helpers.py
@@ -21,7 +21,7 @@ def validate_contact(request, bill, error=True):
         message = msg.format(relation=_("Related"), account=account, url=url)
         send(request, mark_safe(message))
         valid = False
-    main = type(bill).account.field.rel.to.objects.get_main()
+    main = type(bill).account.field.model.objects.get_main()
     if not hasattr(main, 'billcontact'):
         account = force_text(main)
         url = reverse('admin:accounts_account_change', args=(main.id,))

orchestra/contrib/lists/serializers.py
@@ -12,7 +12,7 @@ from .models import List
 
 class RelatedDomainSerializer(AccountSerializerMixin, RelatedHyperlinkedModelSerializer):
     class Meta:
-        model = List.address_domain.field.rel.to
+        model = List.address_domain.field.model
         fields = ('url', 'id', 'name')
 
 
@@ -26,14 +26,14 @@ class ListSerializer(AccountSerializerMixin, SetPasswordHyperlinkedSerializer):
                              'This value may contain any ascii character except for '
                              ' \'/"/\\/ characters.'), 'invalid'),
         ])
-    
+
     address_domain = RelatedDomainSerializer(required=False)
-    
+
     class Meta:
         model = List
         fields = ('url', 'id', 'name', 'password', 'address_name', 'address_domain', 'admin_email', 'is_active',)
         postonly_fields = ('name', 'password')
-    
+
     def validate_address_domain(self, address_name):
         if self.instance:
             address_domain = address_domain or self.instance.address_domain

orchestra/contrib/mailboxes/models.py
@@ -44,7 +44,7 @@ class Mailbox(models.Model):
     def active(self):
         try:
             return self.is_active and self.account.is_active
-        except type(self).account.field.rel.to.DoesNotExist:
+        except type(self).account.field.model.DoesNotExist:
             return self.is_active
 
     def disable(self):

orchestra/contrib/orders/signals.py
@@ -15,7 +15,7 @@ def cancel_orders(sender, **kwargs):
     if sender._meta.app_label not in settings.ORDERS_EXCLUDED_APPS:
         instance = kwargs['instance']
         # Account delete will delete all related orders, no need to maintain order consistency
-        if isinstance(instance, Order.account.field.rel.to):
+        if isinstance(instance, Order.account.field.model):
             return
         if type(instance) in services:
             for order in Order.objects.by_object(instance).active():

orchestra/contrib/saas/services/helpers.py
@@ -42,7 +42,7 @@ def clean_custom_url(saas):
         )
     except Website.DoesNotExist:
         # get or create domain
-        Domain = Website.domains.field.rel.to
+        Domain = Website.domains.field.model
         try:
             domain = Domain.objects.get(name=url.netloc)
         except Domain.DoesNotExist:
@@ -51,7 +51,7 @@ def clean_custom_url(saas):
             })
         if domain.account != account:
             raise ValidationError({
-                'custom_url': _("Domain %s does not belong to account %s, it's from %s.") % 
+                'custom_url': _("Domain %s does not belong to account %s, it's from %s.") %
                     (url.netloc, account, domain.account),
             })
         # Create new website for custom_url
@@ -110,7 +110,7 @@ def create_or_update_directive(saas):
             account=account,
         )
     except Website.DoesNotExist:
-        Domain = Website.domains.field.rel.to
+        Domain = Website.domains.field.model
         domain = Domain.objects.get(name=url.netloc)
         # Create new website for custom_url
         tgt_server = Server.objects.get(name='web.pangea.lan')

orchestra/contrib/systemusers/models.py
@@ -61,7 +61,7 @@ class SystemUser(models.Model):
     def active(self):
         try:
             return self.is_active and self.account.is_active
-        except type(self).account.field.rel.to.DoesNotExist:
+        except type(self).account.field.model.DoesNotExist:
             return self.is_active
 
     @cached_property

commit 48ef1f21e3b679de756aa5aa5e1d2e8619e0c3b5

Navigate through FK field to related model
Fix regression introduced by 7d975637d53d3711b5f8fbe49241d685c4bcf98d
when there is a misunderstanding while replacing deprecated rel.to

orchestra/contrib/accounts/actions.py
@@ -175,7 +175,7 @@ def delete_related_services(modeladmin, request, queryset):
     for model, objs in collector.model_objs.items():
         count = 0
         # discount main systemuser
-        if model is modeladmin.model.main_systemuser.field.model:
+        if model is modeladmin.model.main_systemuser.field.related_model:
             count = len(objs) - 1
         # Discount account
         elif model is not modeladmin.model and model in registered_services:

orchestra/contrib/accounts/forms.py
@@ -47,7 +47,7 @@ def create_account_creation_form():
             # Previous validation error
             return
         errors = {}
-        systemuser_model = Account.main_systemuser.field.model
+        systemuser_model = Account.main_systemuser.field.related_model
         if systemuser_model.objects.filter(username=account.username).exists():
             errors['username'] = _("A system user with this name already exists.")
         for model, key, related_kwargs, __ in create_related:

orchestra/contrib/bills/helpers.py
@@ -21,7 +21,7 @@ def validate_contact(request, bill, error=True):
         message = msg.format(relation=_("Related"), account=account, url=url)
         send(request, mark_safe(message))
         valid = False
-    main = type(bill).account.field.model.objects.get_main()
+    main = type(bill).account.field.related_model.objects.get_main()
     if not hasattr(main, 'billcontact'):
         account = force_text(main)
         url = reverse('admin:accounts_account_change', args=(main.id,))

orchestra/contrib/lists/serializers.py
@@ -12,7 +12,7 @@ from .models import List
 
 class RelatedDomainSerializer(AccountSerializerMixin, RelatedHyperlinkedModelSerializer):
     class Meta:
-        model = List.address_domain.field.model
+        model = List.address_domain.field.related_model
         fields = ('url', 'id', 'name')
 
 

orchestra/contrib/mailboxes/models.py
@@ -44,7 +44,7 @@ class Mailbox(models.Model):
     def active(self):
         try:
             return self.is_active and self.account.is_active
-        except type(self).account.field.model.DoesNotExist:
+        except type(self).account.field.related_model.DoesNotExist:
             return self.is_active
 
     def disable(self):

orchestra/contrib/orders/signals.py
@@ -15,7 +15,7 @@ def cancel_orders(sender, **kwargs):
     if sender._meta.app_label not in settings.ORDERS_EXCLUDED_APPS:
         instance = kwargs['instance']
         # Account delete will delete all related orders, no need to maintain order consistency
-        if isinstance(instance, Order.account.field.model):
+        if isinstance(instance, Order.account.field.related_model):
             return
         if type(instance) in services:
             for order in Order.objects.by_object(instance).active():

orchestra/contrib/saas/services/helpers.py
@@ -42,7 +42,7 @@ def clean_custom_url(saas):
         )
     except Website.DoesNotExist:
         # get or create domain
-        Domain = Website.domains.field.model
+        Domain = Website.domains.field.related_model
         try:
             domain = Domain.objects.get(name=url.netloc)
         except Domain.DoesNotExist:
@@ -110,7 +110,7 @@ def create_or_update_directive(saas):
             account=account,
         )
     except Website.DoesNotExist:
-        Domain = Website.domains.field.model
+        Domain = Website.domains.field.related_model
         domain = Domain.objects.get(name=url.netloc)
         # Create new website for custom_url
         tgt_server = Server.objects.get(name='web.pangea.lan')

orchestra/contrib/systemusers/models.py
@@ -61,7 +61,7 @@ class SystemUser(models.Model):
     def active(self):
         try:
             return self.is_active and self.account.is_active
-        except type(self).account.field.model.DoesNotExist:
+        except type(self).account.field.related_model.DoesNotExist:
             return self.is_active
 
     @cached_property

orchestra/contrib/websites/serializers.py
@@ -13,13 +13,13 @@ from .validators import validate_domain_protocol
 
 class RelatedDomainSerializer(AccountSerializerMixin, RelatedHyperlinkedModelSerializer):
     class Meta:
-        model = Website.domains.field.model
+        model = Website.domains.field.related_model
         fields = ('url', 'id', 'name')
 
 
 class RelatedWebAppSerializer(AccountSerializerMixin, RelatedHyperlinkedModelSerializer):
     class Meta:
-        model = Content.webapp.field.model
+        model = Content.webapp.field.related_model
         fields = ('url', 'id', 'name', 'type')
 
 

orchestra/permissions/options.py
@@ -90,7 +90,7 @@ class RelatedPermission(Permission):
         if obj is None:
             parent = cls
             for relation in relations:
-                parent = getattr(parent, relation).field.model
+                parent = getattr(parent, relation).field.related_model
         else:
             parent = functools.reduce(getattr, relations, obj)
 