b37d9cc51548d1e8e6d8b8e6e65d961b13e2015f
Santiago Lamora <santiago@ribaguifi.com>
2021-07-08 14:48:51 +0200

    Remove unneeded custom 'delete_selected'
    `has_delete_permission` already avoids deleting main system users

--- orchestra/contrib/systemusers/actions.py
+++ orchestra/contrib/systemusers/actions.py
@@ -128,29 +128,3 @@ def create_link(modeladmin, request, queryset):
     return TemplateResponse(request, 'admin/systemusers/systemuser/create_link.html', context)
 create_link.url_name = 'create-link'
 create_link.tool_description = _("Create link")
-
-
-def delete_selected(modeladmin, request, queryset):
-    """ wrapper arround admin.actions.delete_selected to prevent main system users deletion """
-    opts = modeladmin.model._meta
-    app_label = opts.app_label
-    # Check that the user has delete permission for the actual model
-    if not modeladmin.has_delete_permission(request):
-        raise PermissionDenied
-    else:
-        accounts = []
-        for user in queryset:
-            if user.is_main:
-                accounts.append(user.username)
-        if accounts:
-            n = len(accounts)
-            messages.error(request, ungettext(
-                "You have selected one main system user (%(accounts)s), which can not be deleted.",
-                "You have selected some main system users which can not be deleted (%(accounts)s).",
-                n) % {
-                    'accounts': ', '.join(accounts[:10]+['...'] if n > 10 else accounts)
-                }
-            )
-            return
-    return admin.actions.delete_selected(modeladmin, request, queryset)
-delete_selected.short_description = _("Delete selected %(verbose_name_plural)s")
