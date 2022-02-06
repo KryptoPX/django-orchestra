6d5fe544d34abb6122b013754a8e6c8e4bca00f3
KryptoPX <pixelbit2000@gmail.com>
2022-02-06 06:25:03 +0100

    Fix & Update content

--- docker/musician.sh
+++ docker/musician.sh
@@ -7,6 +7,3 @@ pip install -r requirements.txt
 cp .env.example .env
 sed -i 's/https:\/\/api.examplea.org/http:\/\/127.0.0.1:9999\/api/g' .env
 echo "STATIC_ROOT=/tmp/" >> .env
-
-cd django-musician 
-python manage.py runserver 8080
--- docker/orchestra.sh
+++ docker/orchestra.sh
@@ -1,9 +1,10 @@
 . ~/env/bin/activate
+
 git clone https://github.com/ribaguifi/django-orchestra.git
 pip install -r django-orchestra/requirements.txt
 pip install -e django-orchestra
 
 orchestra-admin startproject panel
+
 cd panel
 python manage.py migrate
-python manage.py runserver 9999
